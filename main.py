ab641f42cfaf82b3981cd5a286ec2177cce8ac82
Jason Washburn <wburn@wburn.net>
2021-03-13 06:08:11 -0600

    catches error if user enters something other than a letter

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import pandas as pd
 
 nato_df = pd.read_csv('nato_phonetic_alphabet.csv')  # Read in CSV using Pandas
-nato_dict = {row.letter:row.code for (index, row) in nato_df.iterrows()}  # Create a dict of letters and codes
+nato_dict = {row.letter: row.code for (index, row) in nato_df.iterrows()}  # Create a dict of letters and codes
 
-user_word = input("What is the word you want to convert? ")  # Prompt user for word
-result = [nato_dict[letter] for letter in user_word.upper()]  # Convert user's word into NATO phonetic alphabet
-print(result)
+
+def convert_word():
+
+    user_word = input("What is the word you want to convert? ")  # Prompt user for word
+    try:
+        result = [nato_dict[letter] for letter in user_word.upper()]
+    except KeyError:
+        print("Please enter only letters of the alphabet.")  # Let use know to enter letters only and run it again
+        convert_word()
+    else:
+        print(result)
+
+
+convert_word()
